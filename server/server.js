d3a8bac8c87b1e9aa1556ff790d3289db9d38443
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-06-26 18:59:57 +0700

    Setup Validators, Types and Defaults

--- server/server.js
+++ server/server.js
@@ -3,15 +3,50 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoAppMongoose');
 
+/* User */
+var User = mongoose.model('User', {
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		minglength: 1
+	},
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		default: null,
+	}
+});
+
+var newUser = new User({
+	email: 'maulanayusupp@gmail.com',
+	first_name: 'Maulana Yusup',
+	last_name: 'Abdullah'
+})
+newUser.save().then((doc) => {
+	console.log('Saved user', doc);
+}, (err) => {
+	console.log('Unable to save user', err);
+})
+
+/* Todo */
 var Todo = mongoose.model('Todo', {
 	text: {
-		type: String
+		type: String,
+		required: true,
+		trim: true,
+		minglength: 1
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false
 	},
 	completedAt: {
-		type: Number
+		type: Number,
+		default: null
 	}
 })
 
