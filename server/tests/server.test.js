486454542932f5f57b18d73ffe99afe9a71c01ea
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-06-28 10:58:14 +0700

    Test GET Todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,17 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
+const todos = [{
+	text: 'First test todo'
+},{
+	text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
-})
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
+});
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
@@ -27,7 +35,7 @@ describe('POST /todos', () => {
 				}
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(1);
+					expect(todos.length).toBe(0);
 					expect(todos).toBe(text);
 					done();
 				}).catch((e) => done(e));
@@ -46,8 +54,20 @@ describe('POST /todos', () => {
 			})
 
 			Todo.find().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 	});
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+	})
 });
