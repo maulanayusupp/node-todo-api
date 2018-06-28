af417ce8b28bd223c6413e771a2ab9562b5d35ba
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-06-28 16:34:15 +0700

    Testing - update todo :id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -150,3 +150,44 @@ describe('REMOVE /todos/:id', () => {
 			.end(done);
 	});
 });
+
+/* update todo by id */
+describe('PATCH /todos/:id', () => {
+	it('should update the todo', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		var text = 'this is the new text';
+
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				completed: true,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBe('number');
+			})
+			.end(done());
+	});
+
+	it('should clear completedAt when todo is not completed', (done) => {
+		var hexId = todos[1]._id.toHexString();
+		var text = 'this is the new text!!! completed false';
+
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				completed: false,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toNotExist;
+			})
+			.end(done());
+	});
+});
