c42458a2e33150e3f0e8a2af33269c21c965f19a
DELL-7577-HIZKI\Ramadhan <rizkiailham11@gmail.com>
2019-10-14 19:04:18 +0700

    add server test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -261,4 +261,47 @@ describe('POST /users', () => {
 			.expect(400)
 			.end(done);
 	});
+
+	/* post todos */
+describe('POST /newTodo', () => {
+	it('should create a new todo', (done) => {
+		var text = ' test todo content';
+
+		request(app)
+			.post('/newTodo')
+			.send({text})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.text).toBe(text);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(0);
+					expect(todos).toBe(text);
+					done();
+				}).catch((e) => done(e));
+			})
+	});
+
+	it('should not create a new todo with invalid body data!', (done) => {
+		request(app)
+			.post('/newTodo')
+			.send({})
+			.expect(400)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+			})
+
+			Todo.find().then((todos) => {
+				expect(todos.length).toBe(2);
+				done();
+			}).catch((e) => done(e));
+	});
+});
 })
